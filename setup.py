86979d4f00a95750e500a698099557099c027483
Topsya <topsya1986@gmail.com>
2023-07-07 00:38:16 +0300

    07.07 00.37 robum setup

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     version= "0.1",
     description= " Proekt rabotae s phon book , notebook, clean papki po formats ",
     url='https://github.com/Topsya/teamwork-Tech_Titans',
-    author= "Anton Mescheryakov, Denis Mashtak, Elizaveta Kolesnyk",
-    author_email= "topsya1986@gmail.com,  ned.warov@gmail.com,  kolesnyk.elyzaveta24.1201@gmail.com",
+    author= "Anton Mescheryakov, Denis Mashtak, Elizaveta Kolesnyk, Oleg Dovhyi",
+    author_email= "topsya1986@gmail.com,  ned.warov@gmail.com,  kolesnyk.elyzaveta24.1201@gmail.com,  oleg.dovgyy@gmail.com",
     packages= find_namespace_packages(),
     include_package_data=True,
     entry_points={
