8883de7647931e02259fb40b440326226bc1bbc6
SaL1VaNn <ned.warov@gmail.com>
2023-07-07 17:20:22 +0300

    таск четвертий

---
+++ task_4.py
@@ -0,0 +1,9 @@
+class Contacts:
+    # решта коду класу
+
+    def search_contacts(self, search_term):
+        results = []
+        for contact in self.contacts:
+            if search_term.lower() in contact.name.lower() or search_term.lower() in contact.phone.lower():
+                results.append(contact)
+        return results
