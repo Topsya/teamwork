70d89adebf5b09a45d52badc495be4fa81065ea4
Topsya <topsya1986@gmail.com>
2023-07-09 07:23:41 +0300

    добавили 8 завдання в НотБук

--- NoteBook.py
+++ NoteBook.py
@@ -17,5 +17,18 @@ class NoteBook:
         self.notatki.append(notatki)
 
 
+    def edit_notatki(self, index, notatki):
+        if index >= 0 and index < len(self.notatki):
+            self.notatki[index] = notatki
+        else:
+            raise IndexError("Invalid note index")
+
+    def delete_notatki(self, index):
+        if index >= 0 and index < len(self.notatki):
+            del self.notatki[index]
+        else:
+            raise IndexError("Invalid note index")
+
+
 if __name__ == "__main__":
     zapis = input('add new notes: ')
