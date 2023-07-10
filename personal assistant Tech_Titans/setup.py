f2b0792b5b513413cce29fe7f7a7f82989139905
Topsya <topsya1986@gmail.com>
2023-07-10 22:35:26 +0300

    setup

--- personal assistant Tech_Titans/setup.py
+++ personal assistant Tech_Titans/setup.py
@@ -6,10 +6,19 @@ setup(
     url='https://github.com/Topsya/teamwork-Tech_Titans',
     author= "Anton Mescheryakov, Denis Mashtak, Elizaveta Kolesnyk, Oleg Dovhyi",
     author_email= "topsya1986@gmail.com,  ned.warov@gmail.com,  kolesnyk.elyzaveta24.1201@gmail.com,  oleg.dovgyy@gmail.com",
+    data_files=[('personal assistant Tech_Titans/mygame',['personal assistant Tech_Titans/mygame/background.png',
+                                                          'personal assistant Tech_Titans/mygame/bonus.png',
+                                                          'personal assistant Tech_Titans/mygame/enemy.png',
+                                                           'personal assistant Tech_Titans/mygame/player.png',
+                                                           'personal assistant Tech_Titans/mygame/goose/1-1.png',
+                                                           'personal assistant Tech_Titans/mygame/goose/1-2.png',
+                                                           'personal assistant Tech_Titans/mygame/goose/1-3.png',
+                                                           'personal assistant Tech_Titans/mygame/goose/1-4.png',
+                                                           'personal assistant Tech_Titans/mygame/goose/1-5.png'])],
     packages= find_namespace_packages(),
     include_package_data=True,
     install_requires=['pyowm'],
     entry_points={
-          "console_scripts": ["clean-folder=Clean_folders.start:clean_folder"]
+          'console_scripts': ['assistant=personal assistant Tech_Titans.Menu_project:menu']
           }
 )
