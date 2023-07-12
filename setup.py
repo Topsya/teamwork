a2d625eb5a5ca049eca3f525ca9457bab751f1a4
Topsya <topsya1986@gmail.com>
2023-07-12 22:28:11 +0300

    1111

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_namespace_packages, find_packages 
 setup(
     name="personal_assistant_Tech_Titans010",
-    version= "0.5",
+    version= "0.5.5",
     description= " Proekt rabotae s phon book , notebook, clean papki po formats ",
     url='https://github.com/Topsya/teamwork-Tech_Titans',
     author= "Anton Mescheryakov, Denis Mashtak, Elizaveta Kolesnyk, Oleg Dovhyi",
@@ -15,10 +15,11 @@ setup(
                                                            'personal_assistant_Tech_Titans/mygame/goose/1-3.png',
                                                            'personal_assistant_Tech_Titans/mygame/goose/1-4.png',
                                                            'personal_assistant_Tech_Titans/mygame/goose/1-5.png'])],
-    packages= find_namespace_packages(),
+    # packages=find_namespace_packages(),
+    packages=find_packages(),
     include_package_data=True,
     install_requires=['pyowm','pygame'],
     entry_points={
-          'console_scripts': ['assistant=personal_assistant_Tech_Titans.Menu_project:menu']
+          'console_scripts': ['assistant=personal_assistant_Tech_Titans010.Menu_project:menu']
           }
 )
